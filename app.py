decf445d5e13d65c853f95a38cc3be564d9a325b
boollywoodbot <geetmp3s@gmail.com>
2025-06-20 17:59:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import os
 
 app = Client(
     "cobra_bot",
-    api_id=int(os.environ.get("21905616")),
-    api_hash=os.environ.get("0506d1a8b04f4c580c369b47c885bbd4"),
-    bot_token=os.environ.get("8126788658:AAFd57pq2CiSn0-T1c51ig79ji4k230nrH4")
-)
+api_id = int(os.environ.get("21905616"))
+api_hash = os.environ.get("0506d1a8b04f4c580c369b47c885bbd4")
+bot_token = os.environ.get("8126788658:AAFd57pq2CiSn0-T1c51ig79ji4k230nrH4")
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
