33b688e71398b6e1a27299c66b1effc45b582d4c
boollywoodbot <geetmp3s@gmail.com>
2025-06-20 18:24:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from pyrogram import Client, filters
 import os
 
-# Load .env file **ONLY if** running locally
-if os.path.exists(".env"):
-    from dotenv import load_dotenv
-    load_dotenv()
-
 api_id = int(os.environ.get("21905616"))
 api_hash = os.environ.get("0506d1a8b04f4c580c369b47c885bbd4")
 bot_token = os.environ.get("8126788658:AAFd57pq2CiSn0-T1c51ig79ji4k230nrH4")
