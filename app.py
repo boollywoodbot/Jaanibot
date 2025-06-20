f3d11d08c655b18d9341d38daed8389c4199a687
boollywoodbot <geetmp3s@gmail.com>
2025-06-20 18:13:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from pyrogram import Client, filters
 import os
 
-app = Client(
-    "cobra_bot",
+# Load .env file **ONLY if** running locally
+if os.path.exists(".env"):
+    from dotenv import load_dotenv
+    load_dotenv()
+
 api_id = int(os.environ.get("21905616"))
 api_hash = os.environ.get("0506d1a8b04f4c580c369b47c885bbd4")
 bot_token = os.environ.get("8126788658:AAFd57pq2CiSn0-T1c51ig79ji4k230nrH4")
+
+app = Client(
+    "cobra_bot",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token
+)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply("Hello from Cobra Bot 🐍")
 
-if name == "__main__":
+if __name__ == "__main__":
     app.run()
+
