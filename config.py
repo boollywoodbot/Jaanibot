8be1c73a046717a337e2693ecaf493f0b5dd6c50
boollywoodbot <geetmp3s@gmail.com>
2025-06-20 17:06:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
-# Placeholder for config.py
+mport os
+
+# Required Environment Variables
+API_ID = int(os.environ.get("API_ID", "21905616"))
+API_HASH = os.environ.get("API_HASH", "0506d1a8b04f4c580c369b47c885bbd4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7795598337:AAHVYGMzHr7A2mIhorHHJmyO-qrgByJJIUE")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Txtcobrabot")
+OWNER_ID = int(os.environ.get("OWNER_ID", "7912270773"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
+MONGO_URL = os.environ.get(
+    "MONGO_URL",
+    "mongodb+srv://txtcobra:987654321@cluster0.5xk4pl2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-100"))
+
+# Optional / Supportive
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-100"))  # if needed
+THUMB_URL = os.environ.get("THUMB_URL", "https://envs.sh/9HT.jpg")
+BOT_TEXT = "Txtcobrabot"
+join = '<a href="https://t.me/proex_ch">✳️ Bᴀᴄᴋᴜᴘ</a>'
+UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
+UNSPLASH_QUERY = 'animal baby'
+ADMIN_BOT_USERNAME = "urban_rider2007"  # without @
